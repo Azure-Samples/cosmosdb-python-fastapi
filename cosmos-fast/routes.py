3c450cc173dd794452657d644ed2ce47e6ddf8b3
unknown <abtripathi@microsoft.com>
2022-07-13 19:16:53 +0530

    update list and replace methods

--- cosmos-fast/routes.py
+++ cosmos-fast/routes.py
@@ -14,27 +14,30 @@ def create_todo(request: Request, todoItem: ToDoItem):
     new_todo = request.app.todo_items_container.upsert_item(todoItem)
     return new_todo
 
-@router.get("/", response_description="Fetch all ToDo items", response_model=List[ToDoItem])
+
+@router.get("/", response_model=List[ToDoItem])
 def list_todos(request: Request):
-    query = "SELECT * FROM todo_items"
-    todos = list(request.app.todo_items_container.query_items(query, enable_cross_partition_query=True))
+    todos = list(request.app.todo_items_container.read_all_items())
     return todos
     
+    
+
 
 @router.put("/update", response_model = ToDoItem)
-def update_todo(request: Request, itemId: str, partitionKey: str, itemWithUpdate:UpdateItem):
+def replace_todo(request: Request, itemId: str, partitionKey: str, itemWithUpdate:UpdateItem):
     item = request.app.todo_items_container.read_item(itemId, partition_key=partitionKey)
     itemDict = jsonable_encoder(item)
+    # The update item must contain id and partition key
+    itemWithUpdate.title = itemDict["title"]
+    itemWithUpdate.id = itemDict["id"]
     updateDict = jsonable_encoder(itemWithUpdate)
-    for key in updateDict.keys():
-        if (updateDict[key]) :
-            itemDict[key] = updateDict[key]
-    updatedItem = request.app.todo_items_container.replace_item(itemId, itemDict)    
+    updatedItem = request.app.todo_items_container.replace_item(itemId, updateDict)    
     return updatedItem
 
 
+
 @router.delete("/delete")
-def update_todo(request: Request, itemId: str, partitionKey: str):
+def delete_todo(request: Request, itemId: str, partitionKey: str):
     request.app.todo_items_container.delete_item(itemId, partition_key=partitionKey)
 
 
